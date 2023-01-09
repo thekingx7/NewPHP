245e17e2451e6ea69c7e324e0d6cd7939abf43d6
thekingx7 <108183932+thekingx7@users.noreply.github.com>
2023-01-09 19:04:34 +0300

    Create casting.php

---
+++ casting.php
@@ -0,0 +1,22 @@
+<?php   
+echo gettext(1+1);
+echo "<br>";
+echo gettext(1+(int) "10 tyty");
+echo "<br>";
+echo gettext(1+(integer) "10 tyty");
+
+echo "<br>";
+echo 1+ (int)10.2;
+echo "<br>";
+echo (int) (10.4+10.2); // (منعزله لاكن يعدل القيمه النهايه الي انتجر)
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "<br>";
+?>
